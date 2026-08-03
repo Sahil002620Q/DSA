5a8d337dbf5323d2094b84e8ae2750073ed059e1
Sahil002620Q <smax06388@gmail.com>
2026-08-03 16:51:48 +0530

    prime number in cpp

---
+++ prime.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int in;
+    bool prime = true ;
+    cout << "enter a number : "  ;
+    cin >> in ;
+
+    int i = 2;
+    while (i <= (in - 1)){
+        if(in % i == 0){
+            prime = false;
+            
+        }
+        i++;
+    }
+
+    if (prime == true || prime == 1)
+    {
+        cout << in << " is a prime number " ;
+    }
+    else 
+    {
+        cout << in << " is a composite number" ;
+    }
+}
