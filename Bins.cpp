d03f0818445748734eb895ea33675b15c17e2b2b
Sahil <smax06388@gmail.com>
2026-08-11 22:39:49 +0530

    Add initial binary search setup in Bins.cpp
    Initial implementation of a binary search algorithm with an incomplete target assignment and missing logic.

---
+++ Bins.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int low = 0;
+  int high = 0;
+
+  int arr[] = {1,2,3,4,5,6,7,8,9,10};
+  int size = sizeof(arr)/sizeof(arr[0]);
+  high = size;
+
+  mid = low + high /2;
+  int i = 4;
+  int target = 
+  while(arr[i] != 3
