803b0cffa42dce69963158109f83581eaf983cf6
Sahil002620Q <smax06388@gmail.com>
2026-08-15 20:52:07 +0530

    logics for linear search and binary search in cpp --tier 1

---
+++ Searching_methods/linearsearch.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+void linear_search(int loc,int* arr)
+{
+    int size = sizeof(arr)/sizeof(arr[0]);
+    cout << "size of array is " << size << endl ;
+
+    for(int i = 0 ; i <= size ; i++)
+    {
+        if(arr[i] == loc)
+        {
+            cout << "element " << arr[i] << " is at " << i << endl  ;
+        }
+    } 
+}
+
+int main () 
+{
+    int arr[] = {89,56,23,53,62,90};
+    linear_search(56,arr);
+    // linear_search(23,arr);
+}
