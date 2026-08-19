b72d01f81f711b6a40875af3b0c099e95eacb9a6
Sahil002620Q <smax06388@gmail.com>
2026-08-20 01:15:51 +0530

    check if your given number is prime or composite

--- prime.cpp
+++ prime.cpp
@@ -7,10 +7,11 @@ int main () {
     cin >> in ;
 
     int i = 2;
-    while (i <= (in - 1)){
-        if(in % i == 0){
-            prime = false;
-            
+    while (i <= (in - 1))
+    {
+        if(in % i == 0)
+        {
+            prime = false; 
         }
         i++;
     }
