7280276f5b84d18862d103937cc51611ca0e3202
Sahil002620Q <smax06388@gmail.com>
2026-08-25 23:10:57 +0530

    length of last word ignoring if space is at very last

--- LEETCODE/LengthOfLastWord.cpp
+++ LEETCODE/LengthOfLastWord.cpp
@@ -1,30 +1,36 @@
 #include <iostream>
-#include <vector>
+#include <string>
 using namespace std;
 
 class Solution {
 public:
     int lengthOfLastWord(string s) {
         int len = 0;
-        for(int i = s.length() -1 ; i > 0 ; i--)
+        for(int i = s.length() -1 ; i >= 0 ; i--)
         {
             if(s[i] != ' ' )
             {
                 len++;
             }
-            else if(len != 0 ){
+            else if(len != 0)
+            {
                return len;
+               break;
             }
-        }
+            
+            if(s.length() == 1 || i == 0 && s[i] != ' ' )  return len;
+
+        }    
         return -1;
     }
 };
 
 int main () {
-    string s = "sahil is great ";
+    string s = "luffy is still joyboy ";
     Solution ss;
     int len = ss.lengthOfLastWord(s);
     cout << len ;
+    
 }
 
 //    loop if not ' ' 
