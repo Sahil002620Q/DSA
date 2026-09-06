07ce17a99b064f3a66a243ccad9fdb9e44b61277
Sahil <smax06388@gmail.com>
2026-09-06 22:16:25 +0530

    Create Pattern1.cpp

---
+++ Pattern1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{ 
+    for(int i = 0;i <= 4 ; i ++ )
+    {
+        for(int j = 0 ; j < i ; j ++ )
+        {
+            cout <<j +1 << ' ' ; 
+            
+        }
+        cout << endl ;
+         
+     }       
+}
+     // 00 01 02 03
+     // 10 11 12 13
