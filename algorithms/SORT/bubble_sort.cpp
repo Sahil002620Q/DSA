c8936df7d7fe2c11b51597688b632f73780ac4b4
Sahil002620Q <smax06388@gmail.com>
2026-09-14 23:28:19 +0530

    bubble sort , selections sort and name of other sorting method for future implementation

---
+++ algorithms/SORT/bubble_sort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <utility>
+using namespace std;
+int main ()
+{
+    int arr[] = {1,3,9,3,4,2};
+
+    cout << "original array : " ;
+    for(int x : arr) 
+    {
+        cout << x << ' ' ;
+    }
+
+    int len = sizeof(arr)/sizeof(arr[0]);   //len 6  , loop 0 to 5
+ 
+    for(int i = 0 ; i < len - 1 ; i++ )
+    {
+        for (int j = 0; j < len - 1 - i ; j++) 
+        {
+            if(arr[j] > arr[j +1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+        
+    }
+
+    cout << "\nsorted array : " ;
+    for(int y : arr) {
+        cout << y << ' ' ;
+    }
+    cout << endl ;
+}
